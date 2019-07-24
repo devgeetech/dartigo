39486e27dcb39e37710cb059b5e4110fc831a031
devgeetech <devgeetech@gmail.com>
2019-07-24 16:18:04 +0530

    google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,6 +24,12 @@ module.exports = {
       },
     },
     {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-144503285-1",
+      },
+    },
+    {
       resolve: `gatsby-transformer-remark`,
       options: {
         plugins: [
